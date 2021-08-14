ba007098bcc237a7fd145d0469121805611884d1
Adrian Ornelas <afornela@uci.edu>
2021-08-14 01:39:11 -0700

    1.1.0 Added Linear Correction Term
    Additionally, added all of the documentation, project is now in a  "finished state", if I come across a way to further improve this project, I will likely update it

--- linearwinvolume/__init__.py
+++ linearwinvolume/__init__.py
@@ -1,15 +1,17 @@
 '''linearWinVolume is a package that controls the default sound device's
 volume in Windows in a percentage form as opposed to PyCaw's logarithmic
-scale. After a configuration through linearwinvolume.setup(), win_volume
+scale. After a configuration through linearwinvolume.setup(), linearWinVolume
 accurately converts the dB input to an easily readable 0-100 scale'''
 
 import numpy
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
+from numpy.lib.function_base import _corrcoef_dispatcher
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import os
 import configparser
 import sounddevice as sd
+from scipy.special import lambertw
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
@@ -34,22 +36,56 @@ def setup():
 
     input("This program relies on your input of Windows' volume values; press Enter to open Settings now")
     os.system("start \"\" ms-settings:sound")
-    samples = int(input('Enter maximum number of samples to input: '))
+    samples = int(input('Enter maximum number of samples to input (Recommended: 15+): '))
+    print('') # New line Spacer
     vol_db = [min_vol*(x/samples)+max_vol for x in range(samples)]
     vol_percent = []
     for value in vol_db:
         volume.SetMasterVolumeLevel(value, None)
+        print('Set volume to ',value, 'dB')
         vol_level = float(input('Input Current Volume: '))
         if vol_level != 0:
             vol_percent.append(vol_level)
         else:
             break
     vol_db = vol_db[:len(vol_percent)]
-    log_fit = numpy.polyfit(numpy.log(vol_percent),vol_db,1)
+    coeff, constant = numpy.polyfit(numpy.log(vol_percent),vol_db,1)
+    correction_coeff = 0
+
+    print("\nA logarithmic fit is occasionally not a perfect fit for Windows'")
+    print("volume scaling.\n")
+    ans = input("Would you like to preform a linear correction? (y/n): ").lower()
+
+    if ans == 'y':
+        print("\nEnter any letter if volume displayed MATCHES Windows' Volume")
+        print("otherwise press the Enter key to go to the next value\n")
+        
+        config[sd.query_devices(None,'output')['name']] = {
+        'Natural logarithm coeff':str(coeff),
+        'Constant offset':str(constant),
+        'Correction coeff':str(correction_coeff),
+        'min_vol':str(min_vol),
+        'max_vol':str(max_vol),
+        'samples':str(samples)
+        }
+
+        for i in list(range(100))[::-1]:
+            set_volume(i)
+            ans = input('Guessed Volume Level: {} '.format(i))
+            if ans != '':
+                intersect = i
+                break
+
+        run = 100-intersect
+        rise = max_vol - (constant + coeff * numpy.log(100))
+        correction_coeff = rise/run
+        constant_2 = max_vol - (constant + coeff * numpy.log(100) + correction_coeff * 100)
+        constant = constant + constant_2
 
     config[sd.query_devices(None,'output')['name']] = {
-        'Natural logarithm coeff':str(log_fit[0]),
-        'Constant offset':str(log_fit[1]),
+        'Natural logarithm coeff':str(coeff),
+        'Constant offset':str(constant),
+        'Correction coeff':str(correction_coeff),
         'min_vol':str(min_vol),
         'max_vol':str(max_vol),
         'samples':str(samples)
@@ -72,14 +108,13 @@ def config_check(func):
             print(e)
     return inner
 
-vol_percent = [100.0, 78.0, 60.0, 47.0, 36.0, 28.0, 22.0, 17.0, 13.0, 10.0, 8.0, 6.0, 5.0, 4.0, 3.0, 2.0, 2.0, 1.0, 1.0, 1.0]
-
 @config_check
 def set_volume(value):
     '''Sets the Windows host's volume to a certain percent from the derived logarithmic
     values in the volume.conf'''
     constant = float(config[sd.query_devices(None,'output')['name']]['constant offset'])
     coeff = float(config[sd.query_devices(None,'output')['name']]['natural logarithm coeff'])
+    correction_coeff = float(config[sd.query_devices(None,'output')['name']]['correction coeff'])
     min_vol = float(config[sd.query_devices(None,'output')['name']]['min_vol'])
     max_vol = float(config[sd.query_devices(None,'output')['name']]['max_vol'])
     if value >= 100:
@@ -87,16 +122,26 @@ def set_volume(value):
     elif value <= 0:
         volume.SetMasterVolumeLevel(min_vol, None)
     else:
-        vol = constant + coeff * numpy.log(value)
-        volume.SetMasterVolumeLevel(vol, None)
+        vol = constant + coeff * numpy.log(value) + correction_coeff * value
+        if vol >= max_vol:
+            volume.SetMasterVolumeLevel(max_vol, None)
+        elif vol <= min_vol:
+            volume.SetMasterVolumeLevel(min_vol, None)
+        else:
+            volume.SetMasterVolumeLevel(vol, None)
 
 @config_check
 def get_volume():
     '''Retrieves the Windows host's volume and returns a percent'''
     constant = float(config[sd.query_devices(None,'output')['name']]['constant offset'])
     coeff = float(config[sd.query_devices(None,'output')['name']]['natural logarithm coeff'])
+    correction_coeff = float(config[sd.query_devices(None,'output')['name']]['correction coeff'])
     db = volume.GetMasterVolumeLevel()
-    return int(numpy.exp((db-constant)/coeff))
+
+    # return int(numpy.exp((db-constant)/coeff)) # valid for correction_coeff = 0
+
+    lambert_eval = (correction_coeff * numpy.exp((db/coeff) - (constant/coeff)))
+    return int(round(numpy.real((coeff * lambertw(lambert_eval/coeff))/correction_coeff)))
 
 def change_volume(value):
     '''Changes volume by the input value, can both raise and lower value, if the value goes
--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+'''
+Commands to Update:
+py setup.py sdist
+twine upload dist/*
+'''
+
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as rd:
+  long_description = rd.read()
+
 setup(
  name='linearwinvolume',
- version='1.0.0',
- description="A Python implementation of pycaw that doesn't function on a logarithmic scale",
+ version='1.1.0',
+ description="A Python implementation of pycaw that doesn't function on a decibel scale",
+ long_description=long_description,
+ long_description_content_type="text/markdown",
  url='https://github.com/That-CC/linearWinVolume', 
  author='Adrian Ornelas',
  author_email='afornelas@outlook.com',
  classifiers=[
-   'Intended Audience :: Education',
-   'Operating System :: Windows 10/11',
+   'Operating System :: Microsoft :: Windows',
    'License :: OSI Approved :: MIT License',
    'Programming Language :: Python :: 3',
  ],
  keywords=['python', 'pycaw', 'volume', 'windows volume','windows volume control'],
  packages=find_packages(),
- install_requires = ['pycaw','comtypes', 'enum34;python_version<"3.4"', 'psutil', 'future']
+ install_requires = ['pycaw','comtypes', 'enum34;python_version<"3.4"', 'psutil', 'sounddevice', 'scipy']
 )
